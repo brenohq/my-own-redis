1a6c823bdd1f5480526bc8874c1375c76d3eebac
brenohen <breno.henrique@g.globo>
2024-03-07 19:06:30 -0300

    typo

--- app/server.go
+++ app/server.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port 6379.")
 		os.Exit(1)
 	}
 
