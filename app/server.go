985a46d7810bf4270ed14a4314bcbe95bb0a5bfb
brenohen <breno.henrique@g.globo>
2024-03-07 11:53:15 -0300

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -14,9 +14,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	tcpConn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer tcpConn.Close()
+
+	var res = []byte("+PONG\r\n")
+	_, err = tcpConn.Write(res)
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
 		os.Exit(1)
 	}
 }
