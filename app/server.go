76a3e1c9a519225eb1b4b753bcc76c3dfff03ccd
brenohen <breno.henrique@g.globo>
2024-03-07 18:13:56 -0300

    stage 4 concurrent pings

--- app/server.go
+++ app/server.go
@@ -7,33 +7,40 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
-	tcpConn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer tcpConn.Close()
-
-	for {
-		buf := make([]byte, 1024)
-		_, err := tcpConn.Read(buf)
-		if err != nil {
-			fmt.Println("Error reading:", err.Error())
-			return
-		} else {
-			_, err := tcpConn.Write([]byte("+PONG\r\n"))
+	conns := make(chan net.Conn)
+	go func() {
+		for {
+			conn, err := l.Accept()
 			if err != nil {
-				fmt.Println("Error writing to connection: ", err.Error())
+				fmt.Println("Error accepting connection: ", err.Error())
 				os.Exit(1)
 			}
+			conns <- conn
 		}
+	}()
+
+	defer l.Close()
+
+	for c := range conns {
+		go handleRequest(c)
+	}
+}
+
+func handleRequest(c net.Conn) {
+	readByte := make([]byte, 100)
+
+	for {
+		_, err := c.Read(readByte)
+		if err != nil {
+			fmt.Println("Error Reading: ", err.Error())
+			os.Exit(1)
+		}
+		c.Write([]byte("+PONG\r\n"))
 	}
 }
