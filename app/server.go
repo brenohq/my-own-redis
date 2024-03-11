bf7d0a53ecb81656f54ede882dc4602416c83cc4
brenohen <breno.henrique@g.globo>
2024-03-11 11:55:15 -0300

    pass fifth stage

--- app/server.go
+++ app/server.go
@@ -4,63 +4,55 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"regexp"
+	"strconv"
 	"strings"
 )
 
 func main() {
+	fmt.Println("Logs from your program will appear here!")
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379.")
+		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
-	conns := make(chan net.Conn)
-	go func() {
-		for {
-			conn, err := l.Accept()
-			if err != nil {
-				fmt.Println("Error accepting connection: ", err.Error())
-				os.Exit(1)
-			}
-			conns <- conn
-		}
-	}()
-
-	defer l.Close()
-
-	for c := range conns {
-		go handleRequest(c)
-	}
-}
-
-func handleRequest(c net.Conn) {
 	for {
-		buf := make([]byte, 1024)
-		_, err := c.Read(buf)
+		connection, err := l.Accept()
 
 		if err != nil {
-			fmt.Println("Error Reading: ", err.Error())
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 
-		pattern := regexp.MustCompile(`(\*\d+\r?\n\$\d+\r\n)|(\r?\n\$\d+\r\n)|(\r\n)`)
-		command := strings.Fields(strings.Join(pattern.Split(string(buf), -1), " "))
+		go handleRequest(connection)
+	}
+}
 
-		switch command[0] {
-		case "ping":
-			msg := "+PONG\r\n"
-			_, err := c.Write([]byte(msg))
-			if err != nil {
-				fmt.Println("Error writing to connection: ", err.Error())
-				os.Exit(1)
-			}
+func handleRequest(connection net.Conn) {
+	defer connection.Close()
+
+	for {
+		command_byte := make([]byte, 1024)
+		command_length, err := connection.Read(command_byte)
+
+		if err != nil {
+			fmt.Println("Error reading the request buffer", err)
+			return
+		}
+
+		command := strings.Split(string(command_byte[:command_length]), "\r\n")
+
+		fmt.Println("---------------------------------")
+		fmt.Println(command)
+		fmt.Println("---------------------------------")
+
+		switch strings.ToLower(command[2]) {
 		case "echo":
-			_, err := c.Write([]byte("+" + command[1] + "\r\n"))
-			if err != nil {
-				fmt.Println("Error writing to connection: ", err.Error())
-				os.Exit(1)
-			}
+			connection.Write([]byte("$" + strconv.Itoa(len(command[4])) + "\r\n" + string(strings.Join(command[4:], "\r\n"))))
+		default:
+			connection.Write([]byte("+PONG\r\n"))
 		}
 	}
 }
